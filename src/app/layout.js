7f0812a7e47603ac4bdc165ea5609081f7bb14f3
ranaadeel15911 <ra0511083@gmail.com>
2024-02-27 17:02:28 +0500

    portfolio

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
+import Navbar from "@/app/(components)/Navbar/Navbar"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <NextTopLoader showSpinner={false} zIndex={0}/>
+      <Navbar/>
+        {children}
+        </body>
     </html>
   );
 }
